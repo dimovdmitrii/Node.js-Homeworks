d5525601ae1b12a223a9d24a8b00c580a6727e76
dimovdmitrii <dimovdima1509@gmail.com>
2025-10-05 11:31:39 +0200

    changed product.service.js

--- hw6ExpressSeqielize/hw6.4-5/src/server.js
+++ hw6ExpressSeqielize/hw6.4-5/src/server.js
@@ -8,6 +8,7 @@ const startServer = () => {
   const app = express();
 
   app.use(cors());
+  app.use(express.json());
 
   app.use("/products", productRoutes);
 
--- hw6ExpressSeqielize/hw6.4-5/src/services/products.service.js
+++ hw6ExpressSeqielize/hw6.4-5/src/services/products.service.js
@@ -3,23 +3,17 @@ import sequelize from "../db/sequelize.js";
 
 export const getProducts = async () => {
   const query = `SELECT * FROM products`;
-  const data = await sequelize.query(
-    query,
-    {
-      type: QueryTypes.SELECT,
-    }
-  );
+  const data = await sequelize.query(query, {
+    type: QueryTypes.SELECT,
+  });
 
   return data;
 };
 
 export const addProduct = async ({ name, price }) => {
   const query = `INSERT INTO products (name, price) VALUES('${name}', ${price}) RETURNING *`;
-  const data = await sequelize.query(
-    query,
-    {
-      type: QueryTypes.INSERT
-    }
-  );
+  const data = await sequelize.query(query, {
+    type: QueryTypes.INSERT,
+  });
   return data[0];
 };
