f798975132fefb72bfed64b0a3dc71c167dc15c3
dimovdmitrii <dimovdima1509@gmail.com>
2025-10-22 21:54:48 +0200

    changed product.controllers.js

--- hw12/src/controllers/products.controller.js
+++ hw12/src/controllers/products.controller.js
@@ -9,84 +9,52 @@ import {
 } from "../services/product.service.js";
 
 import { productFullSchema } from "../schemas/product.schema.js";
-import validateBody from "../utils/valodateBody.js";
+import validateBody from "../utils/validateBody.js";
 
 export const getAllProductsController = async (req, res, next) => {
-  try {
-    const result = await getAllProducts();
-    res.json(result);
-  } catch (error) {
-    next(HttpError(500, error.message));
-  }
+  const result = await getAllProducts();
+  res.json(result);
 };
 
 export const getProductByIdController = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await getProductById(id);
+  const { id } = req.params;
+  const result = await getProductById(id);
 
-    if (!result) {
-      throw HttpError(404, `Product with id=${id} not found`);
-    }
-
-    res.json(result);
-  } catch (error) {
-    if (error.name === "CastError" && error.kind === "ObjectId") {
-      return next(HttpError(400, "Invalid product ID format"));
-    }
-
-    next(error.status ? error : HttpError(500, error.message));
+  if (!result) {
+    throw HttpError(404, `Product with id=${id} not found`);
   }
+
+  res.json(result);
 };
 
 export const createProductController = async (req, res, next) => {
-  try {
-    validateBody(productFullSchema, req.body);
+  validateBody(productFullSchema, req.body);
 
-    const result = await createProduct(req.body);
+  const result = await createProduct(req.body);
 
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
+  res.status(201).json(result);
 };
 
 export const updateProductController = async (req, res, next) => {
-  try {
-    validateBody(productFullSchema, req.body);
+  validateBody(productFullSchema, req.body);
 
-    const { id } = req.params;
-    const result = await updateProduct(id, req.body);
+  const { id } = req.params;
+  const result = await updateProduct(id, req.body);
 
-    if (!result) {
-      throw HttpError(404, `Product with id=${id} not found`);
-    }
-
-    res.json(result);
-  } catch (error) {
-    if (error.name === "CastError" && error.kind === "ObjectId") {
-      return next(HttpError(400, "Invalid product ID format"));
-    }
-
-    next(error.status ? error : HttpError(500, error.message));
+  if (!result) {
+    throw HttpError(404, `Product with id=${id} not found`);
   }
+
+  res.json(result);
 };
 
 export const deleteProductController = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await deleteProduct(id);
+  const { id } = req.params;
+  const result = await deleteProduct(id);
 
-    if (!result) {
-      throw HttpError(404, `Product with id=${id} not found`);
-    }
-
-    res.status(204).send();
-  } catch (error) {
-    if (error.name === "CastError" && error.kind === "ObjectId") {
-      return next(HttpError(400, "Invalid product ID format"));
-    }
-
-    next(error.status ? error : HttpError(500, error.message));
+  if (!result) {
+    throw HttpError(404, `Product with id=${id} not found`);
   }
+
+  res.status(204).send();
 };
--- hw12/src/server.js
+++ hw12/src/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import router from "./routers/products.router.js";
 import errorHandler from "./middlewares/errorhandler.js";
 import notFoundHandler from "./middlewares/notFoundHandler.js";
-import HttpError from "./utils/HttpError.js";
 
 const startServer = () => {
   const app = express();
@@ -10,11 +9,9 @@ const startServer = () => {
   app.use(express.json());
 
   app.use("/products", router);
-  app.use((req, res, next) => {
-    next(HttpError(404));
-  });
-  app.use(errorHandler);
+
   app.use(notFoundHandler);
+  app.use(errorHandler);
 
   const port = Number(process.env.PORT);
   app.listen(port, () => console.log(`Server is running on ${port} port`));
